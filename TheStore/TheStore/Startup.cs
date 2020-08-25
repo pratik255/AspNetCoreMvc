e257fef05e3f1bab2b3512fd393efb5df459910c
Pratik Parookaran <pratik255@hotmail.com>
2020-08-25 12:34:34 +0530

    Added mvc to app.

--- TheStore/TheStore/Startup.cs
+++ TheStore/TheStore/Startup.cs
@@ -16,6 +16,7 @@ namespace TheStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,54 @@ namespace TheStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from 1st middleware");
+            //    await next();
+            //    await context.Response.WriteAsync("Bye from 1st middleware");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from 2nd middleware");
+            //    await next();
+            //    await context.Response.WriteAsync("Bye from 2nd middleware");
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        if(env.IsDevelopment())
+            //        {
+            //            await context.Response.WriteAsync("I'm from Dev");
+            //        }
+            //        else if (env.IsStaging())
+            //        {
+            //            await context.Response.WriteAsync("I'm from Stage");
+            //        }
+            //        if (env.IsProduction())
+            //        {
+            //            await context.Response.WriteAsync("I'm from Prod");
+            //        }
+            //        await context.Response.WriteAsync(env.EnvironmentName);
+            //    });
+            //});
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/pratik", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello Pratik!");
+            //    });
+            //});
         }
     }
 }
